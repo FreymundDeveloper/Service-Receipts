264af45cfe300075abbe81a452c6dbdfdf23e593
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-06-07 17:53:44 -0300

    Title Page Ajustment

--- Frontend/nuxt.config.js
+++ Frontend/nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - Frontend',
-    title: 'Frontend',
+    title: 'Service Receipts',
     htmlAttrs: {
       lang: 'en'
     },
