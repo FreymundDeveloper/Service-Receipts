6fbb2dc7d29df0737cb007139b85f47be5199e9d
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-06-07 17:51:33 -0300

    Receipt Table Formatting

--- Frontend/components/ReceiptTable.vue
+++ Frontend/components/ReceiptTable.vue
@@ -11,9 +11,9 @@ export default {
     return {
       headers: [
         { text: 'ID', value: 'id' },
-        { text: 'Cost', value: 'cost' },
-        { text: 'Amount Charged', value: 'amountCharged' },
-        { text: 'Profit', value: 'profit' }
+        { text: 'Cost (US$)', value: 'cost' },
+        { text: 'Amount Charged (US$)', value: 'amountCharged' },
+        { text: 'Profit (US$)', value: 'profit' }
       ],
       items: [],
     };
@@ -25,11 +25,22 @@ export default {
     fetchReceipts() {
       this.$axios.get('https://localhost:7136/Api/Receipt')
         .then(response => {
-          this.items = response.data;
+          this.items = response.data.map(item => ({
+            ...item,
+            cost: this.formatCurrency(item.cost),
+            amountCharged: this.formatCurrency(item.amountCharged),
+            profit: this.formatCurrency(item.profit),
+          }));
         })
         .catch(error => {
           console.error('Error fetching receipts:', error);
         });
+    },
+    formatCurrency(value) {
+      return value.toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
     },
   },
 };
