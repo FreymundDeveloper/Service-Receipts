dc37dd42c7e11756fa5dee6a73379748240939ee
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-06-07 16:44:52 -0300

    Input Data Formatting

--- Frontend/components/FormComponent.vue
+++ Frontend/components/FormComponent.vue
@@ -2,7 +2,14 @@
   <form @submit.prevent="submitForm">
     <v-select v-model="formData.serviceType" :items="serviceTypesItems" :rules="[v => !!v || 'Item is required']" label="Service Type" required ></v-select>
     <v-textarea v-model="formData.description" label="Description" required></v-textarea>
-    <v-text-field v-model="formData.serviceDate" label="Service Date" required></v-text-field>
+     <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :nudge-right="40" transition="scale-transition"
+      offset-y min-width="auto" >
+      <template v-slot:activator="{ on, attrs }">
+        <v-text-field v-model="formData.serviceDate" label="Service Date" prepend-icon="mdi-calendar"
+          readonly v-bind="attrs" v-on="on" required ></v-text-field>
+      </template>
+      <v-date-picker v-model="formData.serviceDate" no-title scrollable @input="menu = false" ></v-date-picker>
+    </v-menu>
     <v-text-field v-model="formData.receipt.cost" label="Cost" required></v-text-field>
     <v-text-field v-model="formData.receipt.amountCharged" label="Amount Charged" required></v-text-field>
     <v-btn type="submit" color="primary">Enviar</v-btn>
@@ -13,9 +20,10 @@
 export default {
   data() {
     return {
+      menu: false,
       formData: {
         description: '',
-        serviceDate: new Date(),
+        serviceDate: new Date().toISOString().slice(0, 10),
         serviceType: null,
         receipt: {
           cost: 0,
