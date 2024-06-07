16054e1141d8133f6192346fe5bd598d64e1620c
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-06-07 17:17:29 -0300

    Reorganization of Rules and Rules with Numeric Types

--- Frontend/components/FormComponent.vue
+++ Frontend/components/FormComponent.vue
@@ -1,17 +1,17 @@
 <template>
   <form @submit.prevent="submitForm">
-    <v-select v-model="formData.serviceType" :items="serviceTypesItems" :rules="[v => !!v || 'Item is required']" label="Service Type" required ></v-select>
-    <v-textarea v-model="formData.description" label="Description" required></v-textarea>
+    <v-select v-model="formData.serviceType" :items="serviceTypesItems" :rules="serviceTypeRules" label="Service Type" required ></v-select>
+    <v-textarea v-model="formData.description" :rules="descriptionRules" label="Description" required></v-textarea>
      <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :nudge-right="40" transition="scale-transition"
-      offset-y min-width="auto" >
+        offset-y min-width="auto">
       <template v-slot:activator="{ on, attrs }">
         <v-text-field v-model="formData.serviceDate" label="Service Date" prepend-icon="mdi-calendar"
-          readonly v-bind="attrs" v-on="on" required ></v-text-field>
+          readonly v-bind="attrs" v-on="on" required></v-text-field>
       </template>
-      <v-date-picker v-model="formData.serviceDate" no-title scrollable @input="menu = false" ></v-date-picker>
+      <v-date-picker v-model="formData.serviceDate" no-title scrollable @input="menu = false"></v-date-picker>
     </v-menu>
-    <v-text-field v-model="formData.receipt.cost" label="Cost" required></v-text-field>
-    <v-text-field v-model="formData.receipt.amountCharged" label="Amount Charged" required></v-text-field>
+    <v-text-field v-model.number="formData.receipt.cost" label="Cost (US$)" required :rules="costRules" @focus="clearCost"></v-text-field>
+    <v-text-field v-model.number="formData.receipt.amountCharged" label="Amount Charged (US$)" required :rules="amountChargedRules" @focus="clearAmountCharged"></v-text-field>
     <v-btn type="submit" color="primary">Enviar</v-btn>
   </form>
 </template>
@@ -37,6 +37,26 @@ export default {
       ],
     };
   },
+  computed: {
+    serviceTypeRules() {
+      return [v => !!v || 'Item is required'];
+    },
+    descriptionRules() {
+      return [v => !!v || 'Item is required'];
+    },
+    costRules() {
+      return [
+        v => !!v || 'Item is required',
+        v => /^[0-9]+(\.[0-9]{1,2})?$/.test(v) || 'Enter a valid number'
+      ];
+    },
+    amountChargedRules() {
+      return [
+        v => !!v || 'Item is required',
+        v => /^[0-9]+(\.[0-9]{1,2})?$/.test(v) || 'Enter a valid number'
+      ];
+    }
+  },
   methods: {
     submitForm() {
       this.$axios.post('https://localhost:7136/Api/Historic', this.formData)
@@ -47,6 +67,16 @@ export default {
           console.error('Error submitting form:', error);
         });
     },
+    clearCost() {
+      if (this.formData.receipt.cost === 0) {
+        this.formData.receipt.cost = null;
+      }
+    },
+    clearAmountCharged() {
+      if (this.formData.receipt.amountCharged === 0) {
+        this.formData.receipt.amountCharged = null;
+      }
+    }
   },
 };
 </script>
